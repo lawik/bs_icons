2c03239ad0210b653406f0c044ee607f27894b38
Lars Wikman <lars@underjord.io>
2023-02-23 11:18:21 +0100

    More horrendous hacks for pulling the SVGs, also features

--- generate.sh
+++ generate.sh
@@ -6,10 +6,12 @@ rm -fr "./$name"
 rm -fr "./priv/png"
 wget "https://github.com/twbs/icons/releases/download/v1.10.3/$name.zip"
 unzip "$name.zip"
-mkdir -p "$name/png"
-cd "$name"
+#mkdir -p "$name/png"
+#mkdir -p "$name/svg"
+#cd "$name"
 
 # requires librsvg-bin or whatever
-for i in $(ls *.svg); do rsvg-convert -h 120 "$i" > "png/${i%.*}.png"; done
+#for i in $(ls *.svg); do rsvg-convert -h 120 -b white "$i" > "png/${i%.*}.png" && cp "$i" "../priv/svg/"; done
 
-mv png ../priv/png
+#mv png ../priv/png
+cp -r "$name" priv/svg
--- lib/bs_icons.ex
+++ lib/bs_icons.ex
@@ -1,13 +1,37 @@
 defmodule BsIcons do
-  @icons File.ls!(Path.join(:code.priv_dir(:bs_icons), "png"))
-         |> Enum.map(&Path.basename(&1, ".png"))
+  {_, 0} = System.shell("./generate.sh")
+
+  @icons File.ls!(Path.join(:code.priv_dir(:bs_icons), "svg"))
+         |> Enum.map(&Path.basename(&1, ".svg"))
          |> Enum.sort()
+
+  @types [:svg]
 
   def all do
     @icons
   end
 
-  def icon_path(name) when name in @icons do
-    Path.join(:code.priv_dir(:bs_icons), "#{name}.png")
+  def svg_icon(name) do
+    name
+    |> icon_path(:svg)
+    |> File.read!()
+  end
+
+  def size(svg, width, height) do
+    svg
+    |> String.replace("width=\"16\"", "width=\"#{width}\"")
+    |> String.replace("height=\"16\"", "height=\"#{height}\"")
+  end
+
+  def color(svg, color) do
+    String.replace(svg, "currentColor", color)
+  end
+
+  def to_png(svg) do
+    Resvg.render(svg, :png)
+  end
+
+  def icon_path(name, type) when name in @icons and type in @types do
+    Path.join(:code.priv_dir(:bs_icons), "#{type}/#{name}.#{type}")
   end
 end
